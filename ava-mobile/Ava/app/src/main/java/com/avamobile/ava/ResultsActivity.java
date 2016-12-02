a3d3c088f87259c6acd349fe611820ec240cc8dd
Viveckh <vpandey@ramapo.edu>
2016-12-02 18:55:27 -0500

    Camera permissions; Added MainActivity, StatsActivity and ResultsActivity

--- ava-mobile/Ava/app/src/main/java/com/avamobile/ava/ResultsActivity.java
+++ ava-mobile/Ava/app/src/main/java/com/avamobile/ava/ResultsActivity.java
@@ -3,13 +3,11 @@ package com.avamobile.ava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_results);
     }
-
-
 }
---
+++ ava-mobile/Ava/app/src/main/java/com/avamobile/ava/StatsActivity.java
@@ -0,0 +1,29 @@
+package com.avamobile.ava;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StatsActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        /*
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        }) ;
+        */
+    }
+}
