3490cbacecbafe9e6afc977bcc0b78afabfd055f
Bishal Regmi <bregmi1@ramapo.edu>
2016-12-02 22:03:28 -0600

    Show me the medicine and I'll tell you what it is

--- ava-server/models/hospital_locator.py
+++ ava-server/models/hospital_locator.py
@@ -1,13 +1,16 @@
 from googleplaces import GooglePlaces, types, lang
+
 API_KEY = "AIzaSyAGGXKLGRwsE2yNDtSVoX6jm4CMTsVzMYU"
-google_places = GooglePlaces(API_KEY)
+
 
 class Hospital_Locator:    
+    
+    google_places = GooglePlaces(API_KEY)
     
     @staticmethod
     def findNearestHospital(location):
         geoLocation = {"lat":location[0],"lng":location[1]}
-        query_result = google_places.nearby_search(
+        query_result = Hospital_Locator.google_places.nearby_search(
         lat_lng=geoLocation, types=[types.TYPE_HOSPITAL]);
         hospital = query_result.places[0];
         hospital.get_details()
