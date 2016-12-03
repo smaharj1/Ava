cc47badaf19ce9590d0aaa6716545dc1a78f52ff
Viveckh <vpandey@ramapo.edu>
2016-12-02 22:25:19 -0500

    Camera integrated to front end and the gradle incompatiblity fixed

--- ava-mobile/Ava/app/src/main/java/com/avamobile/ava/MainActivity.java
+++ ava-mobile/Ava/app/src/main/java/com/avamobile/ava/MainActivity.java
@@ -1,6 +1,5 @@
 package com.avamobile.ava;
 
-<<<<<<< HEAD
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -9,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-=======
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +16,6 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Timer;
 import java.util.TimerTask;
->>>>>>> 16cb2ad9f6a7d29a50bf4f2dcb86f7449ff309d7
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
@@ -26,25 +23,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.entry_logo);
 
-        // On clicking the camera button, start the CameraActivity
-        Button add_medicare = (Button)findViewById(R.id.add_medicare);
-        add_medicare.setOnClickListener(new View.OnClickListener() {
+        // Initiate the animation for the application.
+        new CountDownTimer(3000, 1000) {
             @Override
-            public void onClick(View v) {
-                //Intent cameraIntent =  new Intent(v.getContext(), CameraActivity.class);
-                //cameraIntent.putExtra("startCamera", "true");
-                //startActivity(cameraIntent);
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            public void onTick(long millisUntilFinished) {
             }
-        }) ;
+
+            @Override
+            public void onFinish() {
+                //set the new Content of your activity
+                MainActivity.this.setContentView(R.layout.activity_main);
+
+                // On clicking the camera button, start the Camera and wait for user to take a picture
+                Button add_medicine = (Button) findViewById(R.id.add_medicine);
+                add_medicine.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Intent cameraIntent =  new Intent(v.getContext(), CameraActivity.class);
+                        //cameraIntent.putExtra("startCamera", "true");
+                        //startActivity(cameraIntent);
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    }
+                });
+            }
+        }.start();
     }
 
     /**
      * Serves as a callback for the startActivityForResult()
+     *
      * @param requestCode
      * @param resultCode
      * @param data
@@ -53,33 +63,11 @@ public class MainActivity extends AppCompatActivity {
         //startCamera = false;
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             System.out.println("Got picture");
-            Bitmap bmapPhoto = (Bitmap)data.getExtras().get("data");
+            Bitmap bmapPhoto = (Bitmap) data.getExtras().get("data");
+            Intent intent = new Intent(this, ResultsActivity.class);
+            startActivity(intent);
             //imgViewResult.setImageBitmap(bmapPhoto);
             //System.out.println();
         }
-=======
-        setContentView(R.layout.entry_logo);
-
-        // Initiate the animation for the application.
-        new CountDownTimer(3000,1000){
-            @Override
-            public void onTick(long millisUntilFinished){}
-
-            @Override
-            public void onFinish(){
-                //set the new Content of your activity
-                MainActivity.this.setContentView(R.layout.activity_main);
-            }
-        }.start();
-
-
-        // Handle the countdown for the application. Comes from REST Call
-
-
-    }
-
-    private void run_countdown(){
-
->>>>>>> 16cb2ad9f6a7d29a50bf4f2dcb86f7449ff309d7
     }
 }
