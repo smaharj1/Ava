9134eabb680dadb4d86666eef6d3c490fd22b1eb
Nate <nate@Nates-MacBook-Pro.local>
2016-12-03 12:23:01 -0500

    levenshtein and wine

--- ava-server/models/panic_handler.py
+++ ava-server/models/panic_handler.py
@@ -1,6 +1,7 @@
 from models.twilio_handler import Twilio_Handler
 from models.hospital_locator import Hospital_Locator
 from db.mongo_client import Mongo_Client
+import datetime
 
 class Panic_Handler(object):
 	db = Mongo_Client()
@@ -9,7 +10,7 @@ class Panic_Handler(object):
 	@staticmethod
 	def Panic(location, user):
 		hospital, current_loc = Panic_Handler.getPanicData(location, user)
-		Panic_Handler.textFamily(Panic_Handler.db.GetFamily(user), current_loc)
+		Panic_Handler.textFamily(Panic_Handler.db.GetFamily(user), current_loc, user)
 
 
 
@@ -22,10 +23,11 @@ class Panic_Handler(object):
 
 
 	@staticmethod
-	def textFamily(family, location):
+	def textFamily(family, location,user):
 		for member in family:
-			message = "Hey "+member['name']+"! I am having a medical emergency. I am currently at " \
-					+location[0]['formatted_address'] +". Please help me as soon as possible"
+			message = "Hey "+member['name']+"! This is "+user+". I am having a medical emergency. I am currently at " + \
+					location[0]['formatted_address'] + "\n\nThe authorities have been contacted, but please check in.\n\nSent: " + datetime.datetime.now().strftime('%a, %d %b %X' + \
+						"\n\nIf you are first to respond, text this number your best diagnosis for instructions (Ex. 'heart attack')")
 			Twilio_Handler.sendMessage(member['number'],message);
 
 
---
+++ ava-server/models/procedure_lookup.py
@@ -0,0 +1,28 @@
+import json
+import editdistance
+
+def look_up_procedure(message):
+	message = message.strip().lower()
+	data = {}
+	with open("/Users/nate/Desktop/Ava/ava-server/data/procedures.json") as file:
+		data = json.load(file)
+
+	response = ""
+
+	try:
+		response = data[message]
+		return response
+	except:
+		pass
+
+	keys = data.keys()
+
+	best = ("", 10000000000)
+
+	for key in keys:
+		distance = editdistance.eval(key, message)
+		if distance < best[1]:
+			best = (key, distance)
+
+	return data[best[0]]
+
