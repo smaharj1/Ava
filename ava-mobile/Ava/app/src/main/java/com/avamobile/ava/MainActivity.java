0114eb6ac8ff59ac82446d22867f4261ea949b94
Sujil Maharjan <smaharj1@ramapo.edu>
2016-12-02 19:37:08 -0500

    loading page is done before even starting the app

--- ava-mobile/Ava/app/src/main/java/com/avamobile/ava/MainActivity.java
+++ ava-mobile/Ava/app/src/main/java/com/avamobile/ava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.avamobile.ava;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.entry_logo);
+
+        new CountDownTimer(4000,1000){
+            @Override
+            public void onTick(long millisUntilFinished){}
+
+            @Override
+            public void onFinish(){
+                //set the new Content of your activity
+                MainActivity.this.setContentView(R.layout.activity_main);
+            }
+        }.start();
+
+
     }
 }
